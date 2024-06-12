219012ef4ff95bad93e8f2bb8184ece310ac5a29
syakesoba <soccer.akira6@icloud.com>
2024-06-12 18:42:26 +0900

    add task list display.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
+import { useState } from "react";
+
+type Todo = {
+  value: string;
+};
+
 export const App = () => {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleSubmit = () => {
+    if (!text) return;
+
+    const newTodo: Todo = {
+      value: text,
+    };
+
+    setTodos((todos) => [newTodo, ...todos]);
+    setText('');
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value="" onChange={(e) => e.preventDefault()} />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
         <input
-          type="submit"
-          value="追加"
-          onSubmit={(e) => e.preventDefault()}
+          type="text"
+          value={text}
+          onChange={(e) => handleChange(e)} 
         />
+        <ul>
+          {todos.map((todo) => {
+            return <li>{todo.value}</li>;
+          })}
+        </ul>
+        <input type="submit" value="追加" onSubmit={handleSubmit} />
       </form>
+
+      <p>{text}</p>
     </div>
   );
 };
